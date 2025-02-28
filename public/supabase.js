388acbd27b7cefe088d7963dbdf6b277765f6364
Ahmad Hamza <priahmz@gmail.com>
2025-02-28 01:17:09 +0100

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://your-project-ref.supabase.co',
-  'your-anon-key'
+  'https://cqrnlcrpnyyhxghqwpnm.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxcm5sY3Jwbnl5aHhnaHF3cG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzOTc2OTgsImV4cCI6MjA1NTk3MzY5OH0.ZWYjte3-RHGV3Td7z3cdS3M6toHaMFqJSPJ0iFVd2X4'
 )
 
 export default supabase
